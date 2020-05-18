75835feba12f324daed962c0694d38a2519d0dbf
Yuriy Savchenko <yura_savchenko@outlook.com>
2020-05-18 08:48:52 +0300

    Add handler for Exception

--- src/main/java/com/mycompany/online_shop_demo_backend/controllers/rest/ControllerAdvice.java
+++ src/main/java/com/mycompany/online_shop_demo_backend/controllers/rest/ControllerAdvice.java
@@ -4,15 +4,20 @@ import com.mycompany.online_shop_demo_backend.exceptions.EntityNotFoundException
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class BookControllerAdvice {
+public class ControllerAdvice {
 
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(EntityNotFoundException.class)
     ApiError entityNotFoundException(EntityNotFoundException ex) {
         return new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    ApiError exception(Exception ex) {
+        return new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 }
