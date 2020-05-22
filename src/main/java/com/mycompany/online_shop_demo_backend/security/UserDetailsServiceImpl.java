aa11d2c5edc11043484a6bb435fa998e8841c296
Yuriy Savchenko <yura_savchenko@outlook.com>
2020-05-22 06:25:31 +0300

    Remove redundant default values in UserDetailsServiceImpl

--- src/main/java/com/mycompany/online_shop_demo_backend/security/UserDetailsServiceImpl.java
+++ src/main/java/com/mycompany/online_shop_demo_backend/security/UserDetailsServiceImpl.java
@@ -1,7 +1,6 @@
 package com.mycompany.online_shop_demo_backend.security;
 
 import com.mycompany.online_shop_demo_backend.domain.User;
-import com.mycompany.online_shop_demo_backend.exceptions.EntityNotFoundException;
 import com.mycompany.online_shop_demo_backend.service.db.UserDbService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,16 +17,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userDbService.findByEmail(email)
-                .orElseThrow(() -> new EntityNotFoundException("User with email = " + email + " is not found"));
+        String password = userDbService.findByEmail(email)
+                .map(User::getPassword)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email = " + email + " is not found"));
 
-        return org.springframework.security.core.userdetails
-                .User.withUsername(email)
-                .password(user.getPassword())
-                .accountExpired(false)
-                .accountLocked(false)
-                .credentialsExpired(false)
-                .disabled(false)
+        return org.springframework.security.core.userdetails.User
+                .withUsername(email)
+                .password(password)
                 .authorities(ROLE_USER)
                 .build();
     }
