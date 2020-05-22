e2699edf091cad0b1695e02c3c7e8f6e47ccab7b
Yuriy Savchenko <yura_savchenko@outlook.com>
2020-05-22 06:52:39 +0300

    Update NotAuthorizedException

--- src/main/java/com/mycompany/online_shop_demo_backend/exceptions/NotAuthorizedException.java
+++ src/main/java/com/mycompany/online_shop_demo_backend/exceptions/NotAuthorizedException.java
@@ -1,8 +1,6 @@
 package com.mycompany.online_shop_demo_backend.exceptions;
 
-import org.springframework.security.core.AuthenticationException;
-
-public class NotAuthorizedException extends AuthenticationException {
+public class NotAuthorizedException extends RuntimeException {
 
     public NotAuthorizedException(String message) {
         super(message);
