161333f72f6729c318ce3f3dc2e3029c0180edb5
Yuriy Savchenko <yura_savchenko@outlook.com>
2021-01-11 10:15:33 +0300

    Fix order create error

--- src/main/java/com/mycompany/online_shop_backend/config/AppConfig.java
+++ src/main/java/com/mycompany/online_shop_backend/config/AppConfig.java
@@ -1,11 +1,13 @@
 package com.mycompany.online_shop_backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @Configuration
+@EnableJpaAuditing
 public class AppConfig {
 
     private static final String SERVER_TIMEZONE = "UTC";
--- src/main/java/com/mycompany/online_shop_backend/domain/Order.java
+++ src/main/java/com/mycompany/online_shop_backend/domain/Order.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -18,6 +19,7 @@ import java.util.Set;
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Order {
 
     @Id
