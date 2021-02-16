147a48c5f3fce385c5c8350fc70561dc99949a00
Yuriy Savchenko <yura_savchenko@outlook.com>
2021-02-16 21:14:29 +0300

    Remove @Data for User and Phone

--- src/main/java/com/mycompany/online_shop_backend/domain/User.java
+++ src/main/java/com/mycompany/online_shop_backend/domain/User.java
@@ -1,14 +1,16 @@
 package com.mycompany.online_shop_backend.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Objects;
 
-@Data
 @Entity
 @Table(name = "users")
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
@@ -29,4 +31,28 @@ public class User {
     @Column(name = "password", nullable = false)
     private String password;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
